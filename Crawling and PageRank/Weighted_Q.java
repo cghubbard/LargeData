bedc58a3a43fdee713a0c6687d66e573d9e306fb
cghubbard <hubbard.charles.g@gmail.com>
2016-11-28 14:52:49 -0600

    updates to crawling and pagerank

--- Crawling and PageRank/Weighted_Q.java
+++ Crawling and PageRank/Weighted_Q.java
@@ -3,23 +3,6 @@ package pa3;
 import java.util.PriorityQueue;
 
 public class Weighted_Q {
-
-	public static void main(String[] args) {
-		
-		Tuple t1 = new Tuple("1",0);
-		Tuple t2 = new Tuple("2",6);
-		Tuple t3 = new Tuple("3",0);
-		Weighted_Q q = new Weighted_Q(true);
-		q.add(t1);
-		q.add(t2);
-		q.add(t3);
-		System.out.println(q.extract().getLink());
-		System.out.println(q.extract().getLink());
-		Tuple t4 = new Tuple("4",15);
-		q.add(t4);
-		System.out.println(q.extract().getLink());
-		
-	}
 	
 	
 	private PriorityQueue<Tuple> queue;
